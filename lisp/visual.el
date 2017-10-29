b997643dddf8ad01a19eef39842e503778776ca2
mmgeorge <matt@mattgeorge.net>
2017-10-29 21:25:46 +0000

    Update visual prefs

--- lisp/visual.el
+++ lisp/visual.el
@@ -1,3 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "brightwhite" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(flycheck-error ((t (:foreground "brightred" :underline "brightred"))))
+ '(font-lock-comment-face ((t (:foreground "color-248"))))
+ '(font-lock-function-name-face ((t (:foreground "color-197"))))
+ '(font-lock-keyword-face ((t (:foreground "color-221"))))
+ '(font-lock-string-face ((t (:foreground "color-39"))))
+ '(font-lock-variable-name-face ((t (:foreground "color-197")))))
 
 
 ;; Colors
