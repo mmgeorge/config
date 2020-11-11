f5b27efcbb991a003471e4ba59d786afaf6fb2c7
Matt George <mgeorge@esri.com>
2020-11-10 17:16:48 -0800

    Fix compat 28

--- lisp/visual.el
+++ lisp/visual.el
@@ -1,48 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "brightwhite" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
- '(flycheck-error ((t (:foreground "brightred" :underline "brightred"))))
- '(font-lock-comment-face ((t (:foreground "color-248"))))
- '(font-lock-function-name-face ((t (:foreground "color-197"))))
- '(font-lock-keyword-face ((t (:foreground "color-221"))))
- '(font-lock-string-face ((t (:foreground "color-39"))))
- '(font-lock-variable-name-face ((t (:foreground "color-197")))))
-
-
-;; Colors
-;; (custom-set-variables
-;;  '(ansi-color-faces-vector
-;;    [default default default italic underline success warning error])
-;;  '(custom-enabled-themes (quote (wombat)))
-;;  '(inhibit-startup-screen t)
-;;  '(safe-local-variable-values (quote ((Package . LISP-UNIT)))))
-
-;; (custom-set-faces
-;;  '(custom-variable-tag ((t (:foreground "old lace" :weight bold))))
-;;  '(font-lock-keyword-face ((t (:foreground "gold" :weight bold))))
-;;  '(py-def-class-face ((t (:inherit font-lock-keyword-face :foreground "sandy brown"))))
-;;  '(variable-pitch ((t (:foreground "sky blue" :family "Sans Serif")))))
-
-;; (setq initial-frame-alist
-;;       `((background-color . ,(face-background 'default))
-;;         (foreground-color . ,(face-foreground 'default))
-;;         (horizontal-scroll-bars . nil)
-;;         (top . 50)    
-;;         (left . 550)       
-;;         (height . 58);)
-;;         (cursor-color . "white")
-;;         (mouse-color . "white")))
-
-
 (setq visible-bell 1)
 ;;(set-face-attribute 'default nil :height 100)
 (menu-bar-mode -1)
@@ -51,20 +6,6 @@
 ;; Linum-mode
 (global-linum-mode t)
 (setq linum-format "%d ")
-
-
-;; (defun simple-mode-line-render (left right)
-;;     "Return a string of `window-width' length containing LEFT, and RIGHT
-;;  aligned respectively."
-;;     (let* ((available-width (- (window-width) (length left) 2)))
-;;       (format (format " %%s %%%ds " available-width) left right)))
-
-;; (setq mode-line-format
-;;       '((:eval (simple-mode-line-render
-;;                 ;; left
-;;                 (format-mode-line "%b [%m] [%*]")
-;;                 ;; right
-;;                 (format-mode-line "Line: %l/%i Column: %c")))))
 
 
 (defvar *path-length 5)
@@ -78,13 +19,6 @@
                                  (split-string default-directory "/"))) "/")
       default-directory)))
 
-
-;; (defun shorten-path-when-long2 ()
-;;     (if (> (length default-directory) 35)
-;;         (mapconcat 'identity
-;;                    (cons "{..}"
-;;                          (cddr (cddr (split-string default-directory "/")))) "/")
-;;       default-directory))
 
 (defun mode-line-fill (face reserve)
   "Return empty space using FACE and leaving RESERVE space on the right."
@@ -126,42 +60,6 @@
 
 ;; TABS FORMATING
 
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(ansi-color-faces-vector
-;;    [default default default italic underline success warning error])
-;;  '(custom-enabled-themes (quote (wombat)))
-;;  '(inhibit-startup-screen t)
-;;  '(safe-local-variable-values (quote ((Package . LISP-UNIT)))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(cua-rectangle-noselect ((t (:background "dimgray" :foreground "white"))))
-;;  '(custom-variable-tag ((t (:foreground "old lace" :weight bold))))
-;;  '(flycheck-warning ((t (:underline (:color "red" :style wave)))))
-;;  '(font-lock-function-name-face ((t (:foreground "color-24"))))
-;;  '(font-lock-keyword-face ((t (:foreground "gold" :weight bold))))
-;;  '(font-lock-string-face ((t (:foreground "color-98"))))
-;;  '(font-lock-type-face ((t (:foreground "red" :weight bold))))
-;;  '(helm-selection ((t (:background "color-66" :distant-foreground "color-18"))))
-;;  '(linum ((t (:inherit (shadow default) :foreground "color-102"))))
-;;  '(mode-line ((t (:background "red" :foreground "blue"))))
-;;  '(mode-line-buffer-id ((t (:foreground "color-16"))))
-;;  '(py-def-class-face ((t (:inherit font-lock-keyword-face :foreground "sandy brown"))))
-;;  '(variable-pitch ((t (:foreground "sky blue" :family "Sans Serif")))))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -169,14 +67,27 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "brightwhite" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(flycheck-error ((t (:foreground "brightred" :underline "brightred"))))
- '(font-lock-function-name-face ((t (:foreground "color-197"))))
- '(font-lock-keyword-face ((t (:foreground "color-221"))))
- '(font-lock-string-face ((t (:foreground "color-39"))))
- '(font-lock-variable-name-face ((t (:foreground "color-197")))))
+ '(font-lock-function-name-face ((t (:foreground "deeppink2"))))
+ '(font-lock-keyword-face ((t (:foreground "lightgoldenrod2"))))
+ '(font-lock-string-face ((t (:foreground "deepskyblue1"))))
+ '(font-lock-type-face ((t (:foreground "springgreen1"))))
+ '(font-lock-comment-face ((t (:foreground "snow3"))))
+ '(typescript-jsdoc-tag ((t (:foreground "lightgoldenrod2"))))
+ '(typescript-jsdoc-value ((t (:foreground "ivory1"))))
+ '(font-lock-variable-name-face ((t (:foreground "deeppink2"))))
 
+ `(company-tooltip ((t (:foreground "brightwhite" :background "gray13"))))
+ `(company-tooltip-selection ((t (:background "gray24"))))
+ `(company-tooltip-common ((t (:foreground "brightwhite"))))
+ `(company-tooltip-common-selection ((t (:foreground "brightwhite"))))
 
+ `(company-tooltip-annotation ((t (:foreground "brightwhite"))))
+ `(company-tooltip-annotation-selection ((t (:foreground "brightwhite"))))
+ `(company-scrollbar-fg ((t (:background "brightwhite"))))
+ `(company-scrollbar-bg ((t (:background "gray"))))
 
+ `(helm-buffer-process ((t (:foreground "deepskyblue1"))))
+ `(helm-selection ((t (:background "gray24"))))
+ `(helm-source-header ((t (:foreground "black" :background "gray72"))))
+ `(helm-M-x-key ((t (:foreground "deepskyblue1")))))
 
-
-
-;;; emacs24-root ends here
