76a27c14c78c51773ba5549ddc74754f432cb432
mmgeorge <mattmg412@gmail.com>
2022-01-16 22:17:36 -0800

    Fix configuration issues

--- scripts/install-emacs-native.sh
+++ scripts/install-emacs-native.sh
@@ -4,7 +4,7 @@ sudo apt install libxpm-dev libgif-dev libjpeg-dev libpng-dev libtiff-dev libx11
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo apt install gcc-10 g++-10 libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev
 git clone git://git.sv.gnu.org/emacs.git
-git checkout feature/native-comp
+#git checkout feature/native-comp
 export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
 ./autogen.sh
 ./configure --with-nativecomp --with-json CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
