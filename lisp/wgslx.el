0238f6bd935928a7c2402d0b77dbed9ac1f9c09a
Matt George <mattmg412@gmail.com>
2023-12-22 22:49:08 -0800

    Wgslx support

--- lisp/wgslx.el
+++ lisp/wgslx.el
@@ -1,5 +1,5 @@
 
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "/home/matt/wgslx/target/debug/wgslx")
+ (make-lsp-client :new-connection (lsp-stdio-connection "/home/matt/wgslx/target/debug/wgslx-lsp")
                   :activation-fn (lsp-activate-on "wgsl")
                   :server-id 'wgslx))
