ead5a8b83de32dd6b64af9090ec77bf372793967
Matt George <mattmg412@gmail.com>
2024-04-15 19:40:47 -0700

    Add trouble support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -66,42 +66,44 @@ return {
    
    {
       "folke/trouble.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" },
       opts = {
          position = "bottom", -- position of the list can be: bottom, top, left, right
          height = 10, -- height of the trouble list when position is top or bottom
          width = 50, -- width of the list when position is left or right
-         icons = true, -- use devicons for filenames
+         icons = false, -- use devicons for filenames
          mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-         severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-         fold_open = "", -- icon used for open folds
-         fold_closed = "", -- icon used for closed folds
+         severity = { vim.diagnostic.severity.ERROR,
+                      vim.diagnostic.severity.WARN
+         }, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+         fold_open = "", --"", -- icon used for open folds
+         fold_closed = "", -- "", -- icon used for closed folds
          group = true, -- group results by file
-         padding = true, -- add an extra new line on top of the list
+         padding = false, -- add an extra new line on top of the list
          cycle_results = true, -- cycle item list when reaching beginning or end of list
          action_keys = { -- key mappings for actions in the trouble list
             -- map to {} to remove a mapping, for example:
             -- close = {},
             close = {}, -- close the list
-            cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-            refresh = {}, -- manually refresh
+            -- cancel = "<C-g>", -- cancel the preview and get back to your last window / buffer / cursor
+            refresh = { "r" }, -- manually refresh
             jump = { "<enter>" }, -- jump to the diagnostic or open / close folds
             open_split = {  }, -- open buffer in new split
             open_vsplit = {  }, -- open buffer in new vsplit
             open_tab = {  }, -- open buffer in new tab
             jump_close = {}, -- jump to the diagnostic and close the list
-            toggle_mode = {}, -- toggle between "workspace" and "document" diagnostics mode
-            switch_severity = {}, -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+            toggle_mode = { "p" }, -- toggle between "workspace" and "document" diagnostics mode
+            switch_severity = { }, -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
             toggle_preview = {}, -- toggle auto_preview
             hover = {}, -- opens a small popup with the full multiline message
             preview = {}, -- preview the diagnostic location
             open_code_href = {}, -- if present, open a URI with more information about the diagnostic error
             close_folds = {}, -- close all folds
             open_folds = {}, -- open all folds
-            toggle_fold = {}, -- toggle fold of current file
-            previous = {}, -- previous item
-            next = {}, -- next item
-            help = {}, -- help menu
+            toggle_fold = { "f" }, -- toggle fold of current file
+            previous = { "a" }, -- previous item
+            next = { "d" }, -- next item
+            help = { "?"}, -- help menu
          },
          multiline = true, -- render multi-line messages
          indent_lines = true, -- add an indent guide below the fold icons
@@ -114,11 +116,15 @@ return {
          include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions"  }, -- for the given modes, include the declaration of the current symbol in the results
          signs = {
             -- icons / text used for a diagnostic
-            error = "",
-            warning = "",
-            hint = "",
-            information = "",
-            other = "",
+            -- error = "",
+            -- warning = "",
+            -- hint = "",
+            -- information = "",
+            -- other = "",
+            error = "E",
+            warning = "W",
+            hint = "H",
+            information = "I",
          },
          use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
       },
