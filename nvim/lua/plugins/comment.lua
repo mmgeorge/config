a3ec47af876c8e5091e7b56ceead1374fef0bfe5
Matt George <mattmg412@gmail.com>
2024-04-19 09:07:14 -0700

    Add comment shortcut

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -23,7 +23,7 @@ return {
                  -- Toggle comment on current line
                  -- comment_line = 'i/',
                  -- Toggle comment on visual selection
-                 -- comment_visual = 'i/',
+                 comment_visual = 'i/',
                  -- Define 'comment' textobject (like `dgc` - delete whole comment block)
                  -- Works also in Visual mode if mapping differs from `comment_visual`
                  textobject = '',
