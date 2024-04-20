fc266e56eab9f5f09cba9d3f9f1804152299ea85
Matt George <mattmg412@gmail.com>
2024-04-19 22:34:20 -0700

    Add spider movements

---
+++ nvim/lua/plugins/spider.lua
@@ -0,0 +1,11 @@
+return {
+  "chrisgrieser/nvim-spider", 
+  lazy = true,
+  config = function()
+    require("spider").setup({
+      skipInsignificantPunctuation = true,
+      subwordMovement = true,
+      customPatterns = {}, -- check "Custom Movement Patterns" in the README for details
+    }) 
+  end
+}
