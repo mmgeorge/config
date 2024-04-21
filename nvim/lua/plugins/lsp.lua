aefb9e2943beaddbfcd83ae02b02f63045501f3e
Matt George <mattmg412@gmail.com>
2024-04-21 00:03:34 -0700

    Update to trouble v3

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,196 +1,239 @@
 return {
-   -- Lsp package manager
-   {
-      "williamboman/mason.nvim",
-      config = function()
-         require("mason").setup()
-      end
-   },
+  -- Lsp package manager
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
 
-   -- Bridge mason.nvim & lspconfig
-   {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-         require("mason-lspconfig").setup({
-               ensure_installed = { "lua_ls", "tsserver", "eslint" }
+  -- Bridge mason.nvim & lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "eslint" }
 
-         })
-      end
-   },
-
-   -- Install mason tools automatically -- 
-   {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      config = function()
-         require("mason-tool-installer").setup({
-               ensure_installed = { "stylua", "prettierd" },
-               auto_update = false,
-               run_on_start = true,
-               debounce_hours = 5, 
       })
-      end
-   },
+    end
+  },
 
-   -- Setup servers via lspconfig --
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local lspconfig = require("lspconfig")
-         lspconfig.lua_ls.setup({})
-         lspconfig.rust_analyzer.setup({})
-         lspconfig.tsserver.setup({})
-         lspconfig.eslint.setup({
-               on_attach = function(client, bufnr)
-                  vim.api.nvim_create_autocmd("BufWritePre", {
-                                                 buffer = bufnr,
-                                                 command = "EslintFixAll",
-                  })
-               end,
-               settings = {
-                  codeAction = {
-                     disableRuleComment = {
-                        enable = true,
-                        location = "separateLine"
-                     },
-                     showDocumentation = {
-                        enable = true
-                     }
-                  },
-                  codeActionOnSave = {
-                     enable = true,
-                     mode = "all"
-                  },
-                  experimental = {
-                     useFlatConfig = false
-                  },
-                  format = true,
-                  nodePath = "",
-                  onIgnoredFiles = "off",
-                  problems = {
-                     shortenToSingleLine = false
-                  },
-                  quiet = false,
-                  rulesCustomizations = {
-                     {
-                        rule = 'prettier/prettier',
-                        severity = 'off', 
-                     }
-                  },
-                  run = "onType",
-                  useESLintClass = false,
-                  validate = "on",
-                  workingDirectory = {
-                     mode = "location"
-                  }
-               }
-         })
-      end
-   },
+  -- Install mason tools automatically -- 
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = { "stylua", "prettierd" },
+        auto_update = false,
+        run_on_start = true,
+        debounce_hours = 5, 
+      })
+    end
+  },
 
-   -- Log startup status --
-   {
-      "j-hui/fidget.nvim",
-      opts = {
-         -- options
+  -- Setup servers via lspconfig --
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.eslint.setup({
+        on_attach = function(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+          })
+        end,
+        settings = {
+          codeAction = {
+            disableRuleComment = {
+              enable = true,
+              location = "separateLine"
+            },
+            showDocumentation = {
+              enable = true
+            }
+          },
+          codeActionOnSave = {
+            enable = true,
+            mode = "all"
+          },
+          experimental = {
+            useFlatConfig = false
+          },
+          format = true,
+          nodePath = "",
+          onIgnoredFiles = "off",
+          problems = {
+            shortenToSingleLine = false
+          },
+          quiet = false,
+          rulesCustomizations = {
+            {
+              rule = 'prettier/prettier',
+              severity = 'off', 
+            }
+          },
+          run = "onType",
+          useESLintClass = false,
+          validate = "on",
+          workingDirectory = {
+            mode = "location"
+          }
+        }
+      })
+    end
+  },
+
+  -- Log startup status --
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      -- options
+    },
+  },
+
+  -- {
+  --    "aznhe21/actions-preview.nvim",
+  --    config = function()
+  --       -- vim.keymap.set({ "v", "n" }, "olf", require("actions-preview").code_actions)
+  --    end,
+  -- },
+
+  -- {
+  --    'filipdutescu/renamer.nvim',
+  --    branch = 'master',
+  --    requires = { {'nvim-lua/plenary.nvim'} }
+  -- },
+
+
+  -- Very cool idea, but visual distracting. Make as a toggle?
+  -- Could toggle, see: <https://github.com/ErichDonGubler/lsp_lines.nvim>
+  -- {
+  --    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  --    config = function()
+  --       require("lsp_lines").setup()
+  --    end,
+  -- }
+  {
+
+
+  },
+
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    branch = "dev",
+    keys = {
+      {
+        "we",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      }
+    },
+    opts = {
+      auto_close = false, -- auto close when there are no items
+      auto_open = false, -- auto open when there are items
+      auto_preview = true, -- automatically open preview when on an item
+      auto_refresh = true, -- auto refresh when open
+      focus = false, -- Focus the window when opened
+      restore = true, -- restores the last location in the list when opening
+      follow = true, -- Follow the current item
+      indent_guides = true, -- show indent guides
+      max_items = 200, -- limit number of items that can be displayed per section
+      multiline = true, -- render multi-line messages
+      pinned = false, -- When pinned, the opened trouble window will be bound to the current buffer
+      ---@type trouble.Window.opts
+      win = {}, -- window options for the results window. Can be a split or a floating window.
+      -- Window options for the preview window. Can be a split, floating window,
+      -- or `main` to show the preview in the main editor window.
+      ---@type trouble.Window.opts
+      preview = { type = "main" },
+      -- Throttle/Debounce settings. Should usually not be changed.
+      ---@type table<string, number|{ms:number, debounce?:boolean}>
+      throttle = {
+        refresh = 20, -- fetches new data when needed
+        update = 10, -- updates the window
+        render = 10, -- renders the window
+        follow = 10, -- follows the current item
+        preview = { ms = 100, debounce = true }, -- shows the preview for the current item
       },
-   },
-
-   -- {
-   --    "aznhe21/actions-preview.nvim",
-   --    config = function()
-   --       -- vim.keymap.set({ "v", "n" }, "olf", require("actions-preview").code_actions)
-   --    end,
-   -- },
-
-   -- {
-   --    'filipdutescu/renamer.nvim',
-   --    branch = 'master',
-   --    requires = { {'nvim-lua/plenary.nvim'} }
-   -- },
-
-   
-   -- Very cool idea, but visual distracting. Make as a toggle?
-   -- Could toggle, see: <https://github.com/ErichDonGubler/lsp_lines.nvim>
-   -- {
-   --    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-   --    config = function()
-   --       require("lsp_lines").setup()
-   --    end,
-   -- }
-   {
-      
-      
-   },
-   
-   {
-      "folke/trouble.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
+      -- Key mappings can be set to the name of a builtin action,
+      -- or you can define your own custom action.
+      ---@type table<string, string|trouble.Action>
       keys = {
-         {
-            "we",
-            "<cmd>TroubleToggle<cr>",
-            desc = "Diagnostics (Trouble)",
-         }
+        ["?"] = "help",
+        r = "refresh",
+        R = "toggle_refresh",
+        q = "close",
+        o = "jump_close",
+        ["<esc>"] = "cancel",
+        ["<cr>"] = "jump",
+        ["<2-leftmouse>"] = "jump",
+        ["<c-s>"] = "jump_split",
+        ["<c-v>"] = "jump_vsplit",
+        -- go down to next item (accepts count)
+        -- j = "next",
+        ["}"] = "next",
+        ["]]"] = "next",
+        -- go up to prev item (accepts count)
+        -- k = "prev",
+        ["{"] = "prev",
+        ["[["] = "prev",
+        i = "inspect",
+        p = "preview",
+        P = "toggle_preview",
+        zo = "fold_open",
+        zO = "fold_open_recursive",
+        zc = "fold_close",
+        zC = "fold_close_recursive",
+        za = "fold_toggle",
+        zA = "fold_toggle_recursive",
+        zm = "fold_more",
+        zM = "fold_close_all",
+        zr = "fold_reduce",
+        zR = "fold_open_all",
+        zx = "fold_update",
+        zX = "fold_update_all",
+        zn = "fold_disable",
+        zN = "fold_enable",
+        zi = "fold_toggle_enable",
       },
-      opts = {
-         position = "bottom", -- position of the list can be: bottom, top, left, right
-         height = 15, -- height of the trouble list when position is top or bottom
-         width = 50, -- width of the list when position is left or right
-         icons = true, -- use devicons for filenames
-         mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-         --severity = { vim.diagnostic.severity.ERROR,
-           --           vim.diagnostic.severity.WARN
-         -- }, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-         fold_open = "", --"", -- icon used for open folds
-         fold_closed = "", -- "", -- icon used for closed folds
-         group = true, -- group results by file
-         padding = false, -- add an extra new line on top of the list
-         cycle_results = true, -- cycle item list when reaching beginning or end of list
-         action_keys = { -- key mappings for actions in the trouble list
-            -- map to {} to remove a mapping, for example:
-            -- close = {},
-            close = {}, -- close the list
-            -- cancel = "<C-g>", -- cancel the preview and get back to your last window / buffer / cursor
-            refresh = { "r" }, -- manually refresh
-            jump = { "<enter>" }, -- jump to the diagnostic or open / close folds
-            open_split = {  }, -- open buffer in new split
-            open_vsplit = {  }, -- open buffer in new vsplit
-            open_tab = {  }, -- open buffer in new tab
-            jump_close = {}, -- jump to the diagnostic and close the list
-            toggle_mode = { "p" }, -- toggle between "workspace" and "document" diagnostics mode
-            switch_severity = { }, -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
-            toggle_preview = {}, -- toggle auto_preview
-            hover = {}, -- opens a small popup with the full multiline message
-            preview = {}, -- preview the diagnostic location
-            open_code_href = {}, -- if present, open a URI with more information about the diagnostic error
-            close_folds = {}, -- close all folds
-            open_folds = {}, -- open all folds
-            toggle_fold = { "f" }, -- toggle fold of current file
-            previous = { "a" }, -- previous item
-            next = { "d" }, -- next item
-            help = { "?"}, -- help menu
-         },
-         multiline = false, -- render multi-line messages
-         indent_lines = true, -- add an indent guide below the fold icons
-         win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
-         auto_open = false, -- automatically open the list when you have diagnostics
-         auto_close = false, -- automatically close the list when you have no diagnostics
-         auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-         auto_fold = false, -- automatically fold a file trouble list at creation
-         auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
-         include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions"  }, -- for the given modes, include the declaration of the current symbol in the results
-         signs = {
-            error = "",
-            warning = "",
-            hint = "",
-            information = "",
-            other = "",
-            -- error = "E",
-            -- warning = "W",
-            -- hint = "H",
-            -- information = "I",
-         },
-         use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+      ---@type table<string, trouble.Mode>
+      modes = {
+        symbols = {
+          desc = "document symbols",
+          mode = "lsp_document_symbols",
+          focus = false,
+          win = { position = "right" },
+          filter = {
+            -- remove Package since luals uses it for control flow structures
+            ["not"] = { ft = "lua", kind = "Package" },
+            any = {
+              -- all symbol kinds for help / markdown files
+              ft = { "help", "markdown" },
+              -- default set of symbol kinds
+              kind = {
+                "Class",
+                "Constructor",
+                "Enum",
+                "Field",
+                "Function",
+                "Interface",
+                "Method",
+                "Module",
+                "Namespace",
+                "Package",
+                "Property",
+                "Struct",
+                "Trait",
+              },
+            },
+          },
+        },
       },
-   }
+    } 
+  }
 }
