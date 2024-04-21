d06353c4ae19f9207631cec5a1228f4edd4f702e
Matt George <mattmg412@gmail.com>
2024-04-21 11:43:22 -0700

    Update theming

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,41 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 
+    'projekt0n/github-nvim-theme',
+    'nvim-tree/nvim-web-devicons' },
   config = function()
+    local colors = {
+      red = "#ef3573", 
+      green = "#5bff94",
+      yellow = "#ffe18e",
+      gray = "#696969",
+      white = "#e9e9e9",
+      light_blue = "#3ec5ff",
+      light_gray = "#b4b4b4",
+      teal = "#9dffe7" 
+    }
+    
+    local function getColors(color) 
+      return {
+        a = { bg = color, fg = "black" },
+        b = { bg = "black", fg = color },
+        c = { bg = "black", fg = color }
+      }
+    end 
+
+    local lualine_theme = require('github-theme.util.lualine')('github_dark_default')
+    lualine_theme.normal = getColors(colors.white)
+    lualine_theme.insert = getColors(colors.green)
+    lualine_theme.visual = getColors(colors.red)
+    lualine_theme.command = getColors(colors.light_blue)
+    lualine_theme.replace = getColors(colors.teal)
+    lualine_theme.terminal = getColors(colors.teal)
 
     require('lualine').setup({
+      theme = lualine_theme,
       options = {
-        icons_enabled = false,
-        theme = 'auto',
+        icons_enabled = true,
+        theme = lualine_theme,
         component_separators = { left= '', right= ''}, 
         section_separators = { left= '', right= ''}, 
         -- component_separators = { left = '', right = ''},
@@ -26,9 +55,22 @@ return {
       },
       sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_b = {
+          'branch', 
+          -- 'diff', 
+          { 
+            'diagnostics',
+            sections = { 'error', 'warn' }
+          }
+        },
+        lualine_c = {
+          -- 'filename'
+        },
+        lualine_x = {
+          -- 'encoding', 
+          -- 'fileformat', 
+          'filetype'
+        },
         lualine_y = {'progress'},
         lualine_z = {'location'}
       },
