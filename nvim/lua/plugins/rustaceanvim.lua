d5bc1c444365bb47919fcaf7813f35de2d7436fb
Matt George <mattmg412@gmail.com>
2024-06-11 22:49:17 -0700

    Auto format

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -3,5 +3,33 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^4', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function ()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {
+        },
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            local format_sync_grp = vim.api.nvim_create_augroup("RustaceanFormat", {})
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function() vim.lsp.buf.format() end,
+              group = format_sync_grp,
+            }) 
+            -- you can also put keymaps in here
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {
+        },
+      }
+    end 
   }
 }
