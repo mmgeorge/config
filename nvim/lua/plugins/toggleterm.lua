58b7fe4a6a4e319c99a953d315885d814adecf8b
Matthew George <mgeorge@esri.com>
2024-08-28 11:21:45 -0700

    Add task runner

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    keys = {
+      {
+        "q",
+        "<cmd>:ToggleTerm<cr>",
+        -- desc = "Diagnostics (Trouble)",
+      }
+    },
+    config = {
+      direction = 'float',
+    }
+  }
+}
