b3bd00987fc44b2f43b7372b9e69ad4c3e18f412
Matt George <mattmg412@gmail.com>
2025-05-17 16:54:52 -0700

    feat(treesitter): improve parent node selection and add parent queries for Lua, TSX, and TypeScript
    Refactor parent selection to use lang-specific queries and enhance node retrieval logic.
    Add new 'parent.scm' query files for Lua, TSX, and TypeScript to support improved selection.

---
+++ nvim/queries/lua/parent.scm
@@ -0,0 +1,6 @@
+(([
+   (comment)
+   (function_declaration)
+   (function_definition)
+   ] @_start @_end)
+(#make-range! "range" @_start @_end))
---
+++ nvim/queries/tsx/parent.scm
@@ -0,0 +1,16 @@
+(([
+  (comment)
+  
+  (lexical_declaration
+    (variable_declarator
+      value: (arrow_function)))
+(jsx_element)
+(jsx_self_closing_element)
+(function_declaration)
+(class_declaration)
+(method_definition)
+; typescript
+(type_alias_declaration)
+(interface_declaration)
+] @_start @_end)
+ (#make-range! "parent" @_start @_end))
