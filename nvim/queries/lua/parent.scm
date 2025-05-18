8c5b89f30f3708e27aa053d2d6fce91effafc57b
Matt George <mattmg412@gmail.com>
2025-05-18 15:33:05 -0700

    fix(lua): remove expression_list from parent query
    The expression list was causing false positives
    when detecting parents.

--- nvim/queries/lua/parent.scm
+++ nvim/queries/lua/parent.scm
@@ -4,12 +4,13 @@
   (for_statement)
   (if_statement)
   (else_statement)
-  (expression_list)
+  ; (expression_list)
   (variable_declaration)
   (function_declaration)
   (function_definition)
   (table_constructor)
   ] @parent)
 
+; (expression_list) @list 
 (arguments) @list 
 
