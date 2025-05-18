a32ab98ce8966f59a2015c27725f7c7d5bc2ccc5
Matt George <mattmg412@gmail.com>
2025-05-18 13:50:44 -0700

    feat: Enable taplo LSP and add crates.nvim
    This commit enables the taplo LSP for TOML support, and adds
    crates.nvim for better crate management in Rust projects.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,7 +5,6 @@ return {
       format_after_save = {
         lsp_format = "fallback",
       },
-      json = { "prettier" },
       -- format_on_save = {
       --   -- These options will be passed to conform.format()
       --   timeout_ms = 200,
