3e3a4cdbf2b7d41b99efa0981e3658662bc7b823
Matt George <mattmg412@gmail.com>
2025-05-18 14:39:40 -0700

    feat: Refactor keymaps
    This commit refactors the keymaps to improve consistency. Some keymaps were also deleted, added, or modified.

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -15,19 +15,19 @@ vim.g.clipboard = {
 }
 
 -- Unmaps -
-vim.keymap.set({'n', 'x'}, 'B', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'o', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'q', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'B', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'o', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'q', '<Nop>', {})
 -- keymap({'n', 'v'}, 'xo', '<Nop>')
-vim.keymap.set({'n', 'x'}, 'x', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'b', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'a%', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'w', '<Nop>', {})
-vim.keymap.set({'n', 'x', 'i'}, '<C-z>', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'x', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'b', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'a%', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'w', '<Nop>', {})
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-z>', '<Nop>', {})
 
 vim.keymap.del("n", 'gri', {})
 vim.keymap.del("n", 'grr', {})
-vim.keymap.del({"n", "x"}, 'gra', {})
+vim.keymap.del({ "n", "x" }, 'gra', {})
 vim.keymap.del("n", 'grn', {})
 -- vim.keymap.del({'n', 'x'}, 'grr', {})
 -- vim.keymap.del({'n', 'x'}, 'grn', {})
@@ -47,32 +47,34 @@ local forward = '<C-i>'
 local back = '<C-o>'
 
 for i, v in ipairs(k.keys) do
-  if v ~= '' then 
+  if v ~= '' then
     k.uremap(k.qwerty[i], v)
   end
 end
 
 vim.keymap.del("v", 'p', {})
 vim.keymap.del("v", 'P', {})
+vim.keymap.del("n", "<C-w><C-d>")
+vim.keymap.del("n", "<C-w>d")
 
-vim.keymap.set("v", 'p', function () require("treesitter_smart_select").select_parent() end)
-vim.keymap.set("v", 'P', function () require("treesitter_smart_select").undo_select_parent() end)
+vim.keymap.set("v", 'p', function() require("treesitter_smart_select").select_parent() end)
+vim.keymap.set("v", 'P', function() require("treesitter_smart_select").undo_select_parent() end)
 
-vim.keymap.set({'n', 'x'}, '<iw>', '<Nop>', {})
-vim.keymap.set({'n', 'x'}, 'S', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, '<iw>', '<Nop>', {})
+vim.keymap.set({ 'n', 'x' }, 'S', '<Nop>', {})
 
 -- Semantic keymaps
-vim.keymap.set({'n', 'x'}, '<C-a>', 'ggVG', {})
+vim.keymap.set({ 'n', 'x' }, '<C-a>', 'ggVG', {})
 
-vim.keymap.set({'n', 'x'}, '<Tab>', '==', {})
-vim.keymap.set({'i'}, '<Tab>', '<C-f>', {})
+vim.keymap.set({ 'n', 'x' }, '<Tab>', '==', {})
+vim.keymap.set({ 'i' }, '<Tab>', '<C-f>', {})
 
 vim.keymap.set({ 'n', 'x' }, '<C-p>', '<C-v>')
 
 k.keymap('n', ',', back)
 k.keymap('n', '<', forward)
-k.keymap({'n', 'x'}, 'k', up)
-k.keymap({'n', 'x'}, 'l', down)
+k.keymap({ 'n', 'x' }, 'k', up)
+k.keymap({ 'n', 'x' }, 'l', down)
 
 k.remap('f', se)
 k.remap('a', sb)
@@ -98,54 +100,54 @@ k.keymap('n', 'L', 'o') -- Enter insert below
 -- keymap({'n', 'x'}, 'nzzzv')
 -- keymap({'n', 'x'}, 'Nzzzv')
 
-k.keymap({'n', 'v', 'i'}, '<C-h>', 'dd')
-k.keymap({'n', 'x'}, 'Z', '<C-r>') -- Redo
+k.keymap({ 'n', 'v', 'i' }, '<C-h>', 'dd')
+k.keymap({ 'n', 'x' }, 'Z', '<C-r>') -- Redo
 
 -- vim.keymap.set({'n'}, '<CR>', 'O<ESC>') -- Insert blank line below
-k.keymap({'n'}, 'I', 'O<ESC>') -- Insert blank line below
-k.keymap({'n'}, 'O', 'o<ESC>') -- Insert blank line above
+k.keymap({ 'n' }, 'I', 'O<ESC>') -- Insert blank line below
+k.keymap({ 'n' }, 'O', 'o<ESC>') -- Insert blank line above
 
 -- Move text up or down
-k.keymap({'x'}, 'L', "<CMD>m '<+1<CR>gv=gv") -- Kill
-k.keymap({'x'}, 'K', "<CMD>m '>-2<CR>gv=gv") -- Kill
+k.keymap({ 'x' }, 'L', "<CMD>m '<+1<CR>gv=gv") -- Kill
+k.keymap({ 'x' }, 'K', "<CMD>m '>-2<CR>gv=gv") -- Kill
 
-k.keymap({'n', 'x'}, 'p', 'P') -- P will paste before cursor & over in x without yanking
+k.keymap({ 'n', 'x' }, 'p', 'P')               -- P will paste before cursor & over in x without yanking
 -- k.keymap({'i'}, '<C-p>', '<C-o>p') -- P will paste before cursor & over in x without yanking
-k.keymap({'i'}, '<C-p>', '<C-o>P') -- P will paste before cursor & over in x without yanking
-k.keymap({'x'}, 'u', 'ygv<Esc>') -- Yank selection and move to the end
+k.keymap({ 'i' }, '<C-p>', '<C-o>P')           -- P will paste before cursor & over in x without yanking
+k.keymap({ 'x' }, 'u', 'ygv<Esc>')             -- Yank selection and move to the end
 -- k.keymap({'n', 'x'}, 'uu', 'yy') -- Yank selection and move to the end
-vim.keymap.set({'n'}, "G", 
-  function () require('treesj').toggle() end, { nowait=true, silent=false }) -- Join lines 
-vim.keymap.set({'x', 'n'}, "C", "J", { nowait=true, silent=false }) -- Join lines
+vim.keymap.set({ 'n' }, "G",
+  function() require('treesj').toggle() end, { nowait = true, silent = false }) -- Join lines
+vim.keymap.set({ 'x', 'n' }, "C", "J", { nowait = true, silent = false })       -- Join lines
 -- vim.keymap.set({'n'}, "kM", ":Neogen<CR>", { nowait=true, silent=false }) -- Create doc
 
 -- Surround
-vim.keymap.set({'x'}, "'", "qqc''<Esc>Pq", { nowait=true, silent=false }) 
-vim.keymap.set({'x'}, '"', 'qqc""<Esc>Pq', { nowait=true, silent=false }) 
-vim.keymap.set({'x'}, '(', 'qqc()<Esc>Pq', { nowait=true, silent=false }) 
-vim.keymap.set({'x'}, '<', 'qqc<><Esc>Pq', { nowait=true, silent=false }) 
-vim.keymap.set({'x'}, '[', 'qqc[]<Esc>Pq', { nowait=true, silent=false }) 
-vim.keymap.set({'x'}, '{', 'qqc{}<Esc>Pq', { nowait=true, silent=false }) 
+vim.keymap.set({ 'x' }, "'", "qqc''<Esc>Pq", { nowait = true, silent = false })
+vim.keymap.set({ 'x' }, '"', 'qqc""<Esc>Pq', { nowait = true, silent = false })
+vim.keymap.set({ 'x' }, '(', 'qqc()<Esc>Pq', { nowait = true, silent = false })
+vim.keymap.set({ 'x' }, '<', 'qqc<><Esc>Pq', { nowait = true, silent = false })
+vim.keymap.set({ 'x' }, '[', 'qqc[]<Esc>Pq', { nowait = true, silent = false })
+vim.keymap.set({ 'x' }, '{', 'qqc{}<Esc>Pq', { nowait = true, silent = false })
 
 -- Surround
--- vim.keymap.set({'n'}, "ls'", 'viwqqc""<Esc>Pq', { nowait=true, silent=false }) 
--- vim.keymap.set({'n'}, 'lsa', 'viwqqc()<Esc>Pq', { nowait=true, silent=false }) 
--- vim.keymap.set({'n'}, 'ls,', 'viwqqc<><Esc>Pq', { nowait=true, silent=false }) 
--- vim.keymap.set({'n'}, 'ls[', 'viwqqc[]<Esc>Pq', { nowait=true, silent=false }) 
--- vim.keymap.set({'n'}, 'ls.', 'viwqqc{}<Esc>Pq', { nowait=true, silent=false }) 
+-- vim.keymap.set({'n'}, "ls'", 'viwqqc""<Esc>Pq', { nowait=true, silent=false })
+-- vim.keymap.set({'n'}, 'lsa', 'viwqqc()<Esc>Pq', { nowait=true, silent=false })
+-- vim.keymap.set({'n'}, 'ls,', 'viwqqc<><Esc>Pq', { nowait=true, silent=false })
+-- vim.keymap.set({'n'}, 'ls[', 'viwqqc[]<Esc>Pq', { nowait=true, silent=false })
+-- vim.keymap.set({'n'}, 'ls.', 'viwqqc{}<Esc>Pq', { nowait=true, silent=false })
 
-vim.keymap.set({'n'}, 'zz', 'zf', { nowait=true, silent=false })  -- Add spell check word
+vim.keymap.set({ 'n' }, 'zz', 'zf', { nowait = true, silent = false }) -- Add spell check word
 
 -- Spellcheck
-vim.keymap.set({'n'}, 'Sa', 'zg', { nowait=true, silent=false })  -- Add spell check word
--- vim.keymap.set({'n'}, 'Sr', 'z=', { nowait=true, silent=false }) 
+vim.keymap.set({ 'n' }, 'Sa', 'zg', { nowait = true, silent = false }) -- Add spell check word
+-- vim.keymap.set({'n'}, 'Sr', 'z=', { nowait=true, silent=false })
 -- vim.keymap.set({'x'}, '(', 'S)')
 
 -- Commands
-k.keymap_leader({'x', 'n'}, 'u', "\"+y") -- Copy to clipboard
-k.keymap_leader({'x', 'n'}, 'U', "\"+Y") -- Copy to clipboard
-k.keymap_leader({'x', 'n'}, 'p', "\"+P") -- Paste to clipboard
-k.keymap_leader({'n', 'v'}, 's', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+k.keymap_leader({ 'x', 'n' }, 'u', "\"+y") -- Copy to clipboard
+k.keymap_leader({ 'x', 'n' }, 'U', "\"+Y") -- Copy to clipboard
+k.keymap_leader({ 'x', 'n' }, 'p', "\"+P") -- Paste to clipboard
+k.keymap_leader({ 'n', 'v' }, 's', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 -- k.keymap_leader({'n', 'v'}, 's', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 -- k.keymap_leader({'n', 'v'}, 'iu', function() require("utils").insert_v4({}) end)
 -- k.keymap_leader({'n', 'v'}, 't', 'gg=G')
@@ -153,7 +155,7 @@ k.keymap_leader({'n', 'v'}, 's', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>
 vim.keymap.set("n", "<leader>f", function()
   return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
--- k.keymap_leader({'n'}, 'r', vim.lsp.buf.rename) 
+-- k.keymap_leader({'n'}, 'r', vim.lsp.buf.rename)
 
 -- k.keymap_leader({'n', 'x'}, 'd', function ()
 --   require("telescope.builtin")
@@ -176,32 +178,32 @@ k.keymap('n', 'wo', '<C-w>W')
 -- k.keymap('n', 'wko', '<CMD>only<CR>')
 -- k.keymap('n', 'wks', '<CMD>close!<CR>')
 -- k.keymap('n', 'wkb', '<CMD>bd<CR>')
-vim.keymap.set({'n'}, 'gko', '<CMD>only<CR>', {})
-vim.keymap.set({'n'}, 'gks', '<CMD>close!<CR>', {})
-vim.keymap.set({'n'}, 'gkb', '<CMD>bd<CR>', {})
+vim.keymap.set({ 'n' }, 'gko', '<CMD>only<CR>', {})
+vim.keymap.set({ 'n' }, 'gks', '<CMD>close!<CR>', {})
+vim.keymap.set({ 'n' }, 'gkb', '<CMD>bd<CR>', {})
 
-k.keymap('n', 'wq', function() require("trouble").toggle("quickfix") end) 
+k.keymap('n', 'wq', function() require("trouble").toggle("quickfix") end)
 
 -- Lsp --
 -- keymap('n', '<C-l>f', vim.lsp.buf.code_action)
 
 -- keymap('n', ',', '<C-o>' ) -- Jump back
-k.keymap('n', '=', 'g;' ) -- Jump back to last change
-k.keymap('n', '{', 'g,' ) -- Jump recent change
-k.keymap({'n', 'v'}, '<Tab>', '==' ) 
-vim.keymap.set({'n'}, '<CR>', 'i<CR><ESC>' ) 
+k.keymap('n', '=', 'g;') -- Jump back to last change
+k.keymap('n', '{', 'g,') -- Jump recent change
+k.keymap({ 'n', 'v' }, '<Tab>', '==')
+vim.keymap.set({ 'n' }, '<CR>', 'i<CR><ESC>')
 
-k.keymap('n', '.', '<CMD>Glance definitions<CR>') 
-k.keymap('n', 'or', '<CMD>Glance references<CR>') 
-k.keymap('n', 'ot', '<CMD>Glance type_definitions<CR>') 
-k.keymap({'n', 'x'}, 'ou', function() vim.diagnostic.open_float(nil, { focus = false }) end)
+k.keymap('n', '.', '<CMD>Glance definitions<CR>')
+k.keymap('n', 'or', '<CMD>Glance references<CR>')
+k.keymap('n', 'ot', '<CMD>Glance type_definitions<CR>')
+k.keymap({ 'n', 'x' }, 'ou', function() vim.diagnostic.open_float(nil, { focus = false }) end)
 
-k.keymap('n', 'ou', vim.lsp.buf.hover) 
-k.keymap('n', 'oe', function ()
+k.keymap('n', 'ou', vim.lsp.buf.hover)
+k.keymap('n', 'oe', function()
   vim.diagnostic.open_float(0)
-end) 
--- keymap('n', 'ot', vim.lsp.buf.type_definition) 
--- k.keymap('n', 'ow', vim.lsp.buf.rename) 
+end)
+-- keymap('n', 'ot', vim.lsp.buf.type_definition)
+-- k.keymap('n', 'ow', vim.lsp.buf.rename)
 
 -- k.keymap({'n', 'x'}, '<C-f>', require("plugins.telescope.occur").occur_in_file)
 -- k.keymap({'n', 'x'}, 'of', require("telescope.builtin").find_files)
@@ -212,94 +214,96 @@ end)
 -- k.keymap({'n', 'x'}, 'ows', require("telescope.builtin").lsp_workspace_symbols)
 -- k.keymap({'n', 'x'}, 'opp', vim.lsp.buf.list_workspace_folders)
 -- k.keymap({'n', 'x'}, 'oq', function()
-  -- vim.lsp.buf.format {
-    -- filter = function(client) return client.name ~= "tsserver" end
-  -- }
+-- vim.lsp.buf.format {
+-- filter = function(client) return client.name ~= "tsserver" end
+-- }
 -- end)
 
 -- keymap({'n', 'v'}, 'or', require("telescope.builtin").resume, { nowait=true })
 
 -- Common
-k.keymap({'i', 'v', 'c', 't', 's', 'o', 'x', 't'}, '<C-o>', '<Esc>') -- Cancel
+k.keymap({ 'i', 'v', 'c', 't', 's', 'o', 'x', 't' }, '<C-o>', '<Esc>') -- Cancel
 vim.api.nvim_command(":tnoremap <ESC> <C-\\><C-n>")
 
 k.keymap('n', '<C-_>', '<Plug>(comment_toggle_linewise_current)j') -- Comment line, maps to C-/
 
 k.keymap('n', '<C-e>', function() vim.diagnostic.goto_next() end)
-k.keymap('n', '<C-q>', '<CMD>x<CR>')
+-- k.keymap('n', '<C-q>', '<CMD>x<CR>')
+vim.keymap.set({ 'n' }, '<C-w>', '<CMD>qa!<CR>')
 k.keymap('n', '<S-C-e>', function() vim.diagnostic.goto_prev() end)
-k.keymap('n', '<C-s>', '<CMD>w<CR>') -- Save file
+k.keymap('n', '<C-s>', '<CMD>w<CR>')  -- Save file
 k.keymap('i', '<C-s>', '<C-o>:w<CR>') -- Save file
 
-vim.keymap.set({'n', 'x'}, 'B', function () require"dap".toggle_breakpoint() end, {})
-vim.keymap.set({'n', 'x'}, '<A-a>', function () require"dap".step_over() end, {})
-vim.keymap.set({'n', 'x'}, '<A-i>', function () require"dap".step_into() end, {})
-vim.keymap.set({'n', 'x'}, '<A-n>', function () require"dap".step_out() end, {})
-vim.keymap.set({'n', 'x'}, '<A-r>', function () require"dap".restart() end, {})
-vim.keymap.set({'n', 'x'}, '<A-c>', function () require"dap".continue() end, {})
+
+vim.keymap.set({ 'n', 'x' }, 'B', function() require "dap".toggle_breakpoint() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-a>', function() require "dap".step_over() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-i>', function() require "dap".step_into() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-n>', function() require "dap".step_out() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-r>', function() require "dap".restart() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-c>', function() require "dap".continue() end, {})
 
 -- Custom Motions
 
 -- Select token
-vim.keymap.set({'n'}, 'pt', 'viw', {})
-vim.keymap.set({'n'}, 'lt', 'yiw', {})
-vim.keymap.set({'n'}, 'jt', 'diw', {})
-vim.keymap.set({'n'}, 'mt', 'ciw', {})
+vim.keymap.set({ 'n' }, 'pt', 'viw', {})
+vim.keymap.set({ 'n' }, 'lt', 'yiw', {})
+vim.keymap.set({ 'n' }, 'jt', 'diw', {})
+vim.keymap.set({ 'n' }, 'mt', 'ciw', {})
 -- vim.keymap.set({'n'}, 'pot', 'vaw', {})
 -- vim.keymap.set({'n'}, 'lot', 'yaw', {})
 -- vim.keymap.set({'n'}, 'jot', 'daw', {})
 -- vim.keymap.set({'n'}, 'mot', 'caw', {})
 
 -- Select "
-vim.keymap.set({'n'}, 'po"', 'va"', {})
-vim.keymap.set({'n'}, 'lo"', 'ya"', {})
-vim.keymap.set({'n'}, 'jo"', 'da"', {})
-vim.keymap.set({'n'}, 'mo"', 'ca"', {})
+vim.keymap.set({ 'n' }, 'po"', 'va"', {})
+vim.keymap.set({ 'n' }, 'lo"', 'ya"', {})
+vim.keymap.set({ 'n' }, 'jo"', 'da"', {})
+vim.keymap.set({ 'n' }, 'mo"', 'ca"', {})
 
 -- Select '
-vim.keymap.set({'n'}, 'po\'', 'va\'', {})
-vim.keymap.set({'n'}, 'lo\'', 'ya\'', {})
-vim.keymap.set({'n'}, 'jo\'', 'da\'', {})
-vim.keymap.set({'n'}, 'mo\'', 'ca\'', {})
+vim.keymap.set({ 'n' }, 'po\'', 'va\'', {})
+vim.keymap.set({ 'n' }, 'lo\'', 'ya\'', {})
+vim.keymap.set({ 'n' }, 'jo\'', 'da\'', {})
+vim.keymap.set({ 'n' }, 'mo\'', 'ca\'', {})
 
 -- Select parens
-vim.keymap.set({'n'}, 'pua', 'vi(', {})
-vim.keymap.set({'n'}, 'poa', 'va(', {})
-vim.keymap.set({'n'}, 'lua', 'yi(', {})
-vim.keymap.set({'n'}, 'loa', 'ya(', {})
-vim.keymap.set({'n'}, 'jua', 'di(', {})
-vim.keymap.set({'n'}, 'joa', 'da(', {})
-vim.keymap.set({'n'}, 'mua', 'ci(', {})
-vim.keymap.set({'n'}, 'moa', 'ca(', {})
+vim.keymap.set({ 'n' }, 'pua', 'vi(', {})
+vim.keymap.set({ 'n' }, 'poa', 'va(', {})
+vim.keymap.set({ 'n' }, 'lua', 'yi(', {})
+vim.keymap.set({ 'n' }, 'loa', 'ya(', {})
+vim.keymap.set({ 'n' }, 'jua', 'di(', {})
+vim.keymap.set({ 'n' }, 'joa', 'da(', {})
+vim.keymap.set({ 'n' }, 'mua', 'ci(', {})
+vim.keymap.set({ 'n' }, 'moa', 'ca(', {})
 
 -- Select {
-vim.keymap.set({'n'}, 'pu.', 'vi{', {})
-vim.keymap.set({'n'}, 'po.', 'va{', {})
-vim.keymap.set({'n'}, 'lu.', 'yi{', {})
-vim.keymap.set({'n'}, 'lo.', 'ya{', {})
-vim.keymap.set({'n'}, 'ju.', 'di{', {})
-vim.keymap.set({'n'}, 'jo.', 'da{', {})
-vim.keymap.set({'n'}, 'mu.', 'ci{', {})
-vim.keymap.set({'n'}, 'mo.', 'ca{', {})
+vim.keymap.set({ 'n' }, 'pu.', 'vi{', {})
+vim.keymap.set({ 'n' }, 'po.', 'va{', {})
+vim.keymap.set({ 'n' }, 'lu.', 'yi{', {})
+vim.keymap.set({ 'n' }, 'lo.', 'ya{', {})
+vim.keymap.set({ 'n' }, 'ju.', 'di{', {})
+vim.keymap.set({ 'n' }, 'jo.', 'da{', {})
+vim.keymap.set({ 'n' }, 'mu.', 'ci{', {})
+vim.keymap.set({ 'n' }, 'mo.', 'ca{', {})
 
 -- Select [
-vim.keymap.set({'n'}, 'pu/', 'vi[', {})
-vim.keymap.set({'n'}, 'po/', 'va[', {})
-vim.keymap.set({'n'}, 'lu/', 'yi[', {})
-vim.keymap.set({'n'}, 'lo/', 'ya[', {})
-vim.keymap.set({'n'}, 'ju/', 'di[', {})
-vim.keymap.set({'n'}, 'jo/', 'da[', {})
-vim.keymap.set({'n'}, 'mu/', 'ci[', {})
-vim.keymap.set({'n'}, 'mo/', 'ca[', {})
+vim.keymap.set({ 'n' }, 'pu/', 'vi[', {})
+vim.keymap.set({ 'n' }, 'po/', 'va[', {})
+vim.keymap.set({ 'n' }, 'lu/', 'yi[', {})
+vim.keymap.set({ 'n' }, 'lo/', 'ya[', {})
+vim.keymap.set({ 'n' }, 'ju/', 'di[', {})
+vim.keymap.set({ 'n' }, 'jo/', 'da[', {})
+vim.keymap.set({ 'n' }, 'mu/', 'ci[', {})
+vim.keymap.set({ 'n' }, 'mo/', 'ca[', {})
 
 -- Select <
-vim.keymap.set({'n'}, 'pu,', 'vi<', {})
-vim.keymap.set({'n'}, 'po,', 'va<', {})
-vim.keymap.set({'n'}, 'lu,', 'yi<', {})
-vim.keymap.set({'n'}, 'lo,', 'ya<', {})
-vim.keymap.set({'n'}, 'ju,', 'di<', {})
-vim.keymap.set({'n'}, 'jo,', 'da<', {})
-vim.keymap.set({'n'}, 'mu,', 'ci<', {})
-vim.keymap.set({'n'}, 'mo,', 'ca<', {})
+vim.keymap.set({ 'n' }, 'pu,', 'vi<', {})
+vim.keymap.set({ 'n' }, 'po,', 'va<', {})
+vim.keymap.set({ 'n' }, 'lu,', 'yi<', {})
+vim.keymap.set({ 'n' }, 'lo,', 'ya<', {})
+vim.keymap.set({ 'n' }, 'ju,', 'di<', {})
+vim.keymap.set({ 'n' }, 'jo,', 'da<', {})
+vim.keymap.set({ 'n' }, 'mu,', 'ci<', {})
+vim.keymap.set({ 'n' }, 'mo,', 'ca<', {})
 
-vim.keymap.set({'n', 'x'}, '<A-c>', function () require"dap".continue() end, {})
+vim.keymap.set({ 'n', 'x' }, '<A-c>', function() require "dap".continue() end, {})
