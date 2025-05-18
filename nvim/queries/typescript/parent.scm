4a0ac12511eacab5b3015081c15e09d0b5908b44
Matt George <mattmg412@gmail.com>
2025-05-18 02:42:37 -0700

    feat: enhance treesitter node selection and expand TypeScript query parents
    Improve the node selection by properly extending the end column to include trailing
    punctuation and whitespace, and adjust the selection boundary to avoid inclusion errors.
    
    Expand the TypeScript parent query to include more syntax nodes such as comments,
    export statements, and control flow statements for better selection functionality.

--- nvim/queries/typescript/parent.scm
+++ nvim/queries/typescript/parent.scm
@@ -1,10 +1,31 @@
-([
-  (function_declaration)
-  (lexical_declaration)
-  (class_declaration)
-  (method_definition)
-  ; typescript
-  (type_alias_declaration)
-  (interface_declaration)
-  ] @parent)
+(([
+   (comment)
+   (export_statement)
+   (lexical_declaration)
+   (if_statement)
+   (else_clause)
+   (switch_statement)
+   (switch_case)
+   (switch_default)
+
+   ; (lexical_declaration
+   ;   (variable_declarator
+   ;     value: (arrow_function)))
+   (function_declaration)
+   (class_declaration)
+   (method_definition)
+   ; typescript
+   (type_alias_declaration)
+   (interface_declaration)
+   ] @parent))
+   
+(else_clause) @jump  
+(export_statement) @jump 
+
+(identifier) @list_arg 
+(required_parameter) @list_arg 
+(optional_parameter) @list_arg 
+
+(array) @list 
+(formal_parameters) @list 
 
