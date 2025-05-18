05f6161699ef0dbb1fb556e5922bab7fd7dd2d64
Matt George <mattmg412@gmail.com>
2025-05-18 16:28:18 -0700

    feat: Enable eslint and conform for all filetypes
    The eslint lsp is configured and conform is set up to format
    all filetypes with lsp and trim whitespace.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,6 +5,11 @@ return {
       format_after_save = {
         lsp_format = "fallback",
       },
+      -- tsx = { "trim_whitespace" },
+      -- tsx = { "trim_whitespace" },
+      formatters_by_ft  = {
+        ["*"] = { lsp_format,  "trim_whitespace"  },
+      }
       -- format_on_save = {
       --   -- These options will be passed to conform.format()
       --   timeout_ms = 200,
