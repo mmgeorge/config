27a045a4729f50f7d1ce0926e7695f6f26d334da
Matt George <mattmg412@gmail.com>
2025-05-18 22:48:09 -0700

    feat: Update AI models and disable vtsls
    Updates the AI plugin with new gemini models.
    Disables vtsls due to import issues.

--- nvim/queries/typescript/parent.scm
+++ nvim/queries/typescript/parent.scm
@@ -14,14 +14,36 @@
    (method_definition)
    (type_alias_declaration)
    (interface_declaration)
+   (property_signature)
+   (public_field_definition)
+   (pair)
+   (object)
+   (template_string)
+   (string)
+   (string_fragment)
+   (call_expression)
+
+   (array)
+   (object)
+  
+   ;; change padding capture for theses:
+   (array_pattern)
+   (generic_type)
+   (union_type)
+   (object_type)
+   (identifier)
+   (array_type)
+   (type_identifier)
    ] @parent))
-   
+
 (else_clause) @jump  
 (export_statement) @jump 
 ; (identifier) @list_arg 
 ; (required_parameter) @list_arg 
 ; (optional_parameter) @list_arg 
 
+(for_statement) @list 
+(type_arguments) @list 
 (array) @list 
 (formal_parameters) @list 
 
