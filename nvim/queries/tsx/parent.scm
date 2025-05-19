e23e6b430714b82d9cd73f8afab8a6298b7eeb26
Matt George <mattmg412@gmail.com>
2025-05-19 00:45:53 -0700

    feat(treesitter_smart_select): improve node selection and whitespace handling
    This commit improves node selection and whitespace handling
    in treesitter_smart_select.lua and parent.scm.

--- nvim/queries/tsx/parent.scm
+++ nvim/queries/tsx/parent.scm
@@ -1,6 +1,7 @@
 (([
    (comment)
    (export_statement)
+   (return_statement)
    (lexical_declaration)
    (if_statement)
    (else_clause)
@@ -27,6 +28,8 @@
    
    (array)
    (object)
+   (type_parameters) 
+   (array_pattern)
 
    ;; change padding capture for theses:
    (array_pattern)
@@ -36,6 +39,7 @@
    (identifier)
    (array_type)
    (type_identifier)
+   (predefined_type)
    ] @parent))
 
 (else_clause) @jump  
@@ -44,8 +48,10 @@
 ; (required_parameter) @list_arg 
 ; (optional_parameter) @list_arg 
 
+(array_pattern) @list
 (for_statement) @list 
 (type_arguments) @list 
 (array) @list 
 (formal_parameters) @list 
+(type_parameters) @list
 
