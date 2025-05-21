12076f0345da06059eaa4ea130b6015413f93333
Matt George <mattmg412@gmail.com>
2025-05-20 22:30:41 -0700

    feat(typescript): Add shorthand_property_identifier to parent query
    This adds `shorthand_property_identifier` to the parent query.

--- nvim/queries/typescript/parent.scm
+++ nvim/queries/typescript/parent.scm
@@ -1,54 +1,60 @@
-(([
-   (comment)
-   (export_statement)
-   (return_statement)
-   (lexical_declaration)
-   (if_statement)
-   (else_clause)
-   (switch_statement)
-   (switch_case)
-   (switch_default)
-   (array)
-   (for_statement)
-   (function_declaration)
-   (class_declaration)
-   (method_definition)
-   (type_alias_declaration)
-   (interface_declaration)
-   (property_signature)
-   (public_field_definition)
-   (pair)
-   (object)
-   (template_string)
-   (string)
-   (string_fragment)
-   (call_expression)
+[
+  (comment)
+  (export_statement)
+  (return_statement)
+  (lexical_declaration)
+  (if_statement)
+  (else_clause)
+  (switch_statement)
+  (switch_case)
+  (switch_default)
+  (array)
+  (for_statement)
+  (function_declaration)
+  (class_declaration)
+  (method_definition)
+  (type_alias_declaration)
+  (interface_declaration)
+  (property_signature)
+  (public_field_definition)
+  (pair)
+  (object)
+  (template_string)
+  (string)
+  (string_fragment)
+  (call_expression)
+  (array)
+  (object)
+  (type_parameters)
+  (array_pattern)
+  ; change padding capture for theses:
+  (array_pattern)
+  (generic_type)
+  (union_type)
+  (object_type)
+  (identifier)
+  (array_type)
+  (shorthand_property_identifier)
+  (type_identifier)
+  (predefined_type)
+] @parent
 
-   (array)
-   (object)
-   (type_parameters) 
-   (array_pattern)
-   
-   ;; change padding capture for theses:
-   (array_pattern)
-   (generic_type)
-   (union_type)
-   (object_type)
-   (identifier)
-   (array_type)
-   (type_identifier)
-   (predefined_type)
-   ] @parent))
+(else_clause) @jump
 
-(else_clause) @jump  
-(export_statement) @jump 
-; (identifier) @list_arg 
-; (required_parameter) @list_arg 
-; (optional_parameter) @list_arg 
+(export_statement) @jump
 
 (array_pattern) @list
-(for_statement) @list 
-(type_arguments) @list 
-(array) @list 
-(formal_parameters) @list 
+
+(arguments) @list
+
+(for_statement) @list
+
+(type_arguments) @list
+
+(object) @list
+
+(array) @list
+
+(formal_parameters) @list
+
 (type_parameters) @list
