023bace33e25a884e26f861d8fdbc955c5471ec1
Matt George <mattmg412@gmail.com>
2025-05-21 20:16:45 -0700

    feat(treesitter): improve smart select
    Adds chain selections and jump capture. Also fixes some selection issues.

--- nvim/queries/rust/parent.scm
+++ nvim/queries/rust/parent.scm
@@ -1,41 +1,18 @@
-[
-  (use_declaration)
-  (self_parameter)
-  (parameter)
-  (dynamic_type)
-  (line_comment)
-  (attribute_item)
-  (field_declaration)
-  (enum_variant)
-  (expression_statement)
-  (match_arm)
-  (match_pattern)
-  (generic_function)
-  (macro_definition)
-  (function_item)
-  (function_signature_item)
-  (struct_item)
-  (enum_item)
-  (impl_item)
-  (let_declaration)
-  (call_expression)
-  (macro_invocation)
-  (scoped_identifier)
-  (reference_type)
-  (reference_expression)
-  (lifetime)
-  (trait_bounds)
-  (abstract_type) ; impl Iterator<..>
-  ; (generic_type)
-  ; (type_identifier)
-  (string_literal)
-  (string_content)
-  (field_initializer_list)
-  ; Select inner -- all list types?
-  (array_expression)
-] @parent
+(call_expression) @outer_only
+
+(field_identifier) @chain
+
+(field_expression) @jump
+
+(call_expression
+  function: (identifier) @jump)
+
+(macro_invocation
+  macro: (identifier) @jump)
 
 (field_initializer_list) @list
+
+(field_declaration_list) @list
 
 (token_tree) @list
 
