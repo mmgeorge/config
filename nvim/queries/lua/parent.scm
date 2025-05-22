4adb4483bb8039b7c1d1fc7256e14a5a70eca7f5
Matt George <mattmg412@gmail.com>
2025-05-21 22:19:35 -0700

    feat(treesitter_smart_select): improve parent selection
    This change improves parent selection logic. It also introduces
    new queries for elseif and if statements.

--- nvim/queries/lua/parent.scm
+++ nvim/queries/lua/parent.scm
@@ -1,21 +1,20 @@
-[
-  (comment)
-  (field)
-  (for_statement)
-  (if_statement)
-  (while_statement)
-  (else_statement)
-  ; (expression_list)
-  (variable_declaration)
-  (function_declaration)
-  (function_definition)
-  (function_call)
-  (table_constructor)
-  (assignment_statement)
-  (dot_index_expression)
-  (string)
-  (string_content)
-] @parent
-
+; [
+;   (comment)
+;   (field)
+;   (for_statement)
+;   (if_statement)
+;   (while_statement)
+;   (else_statement)
+;   ; (expression_list)
+;   (variable_declaration)
+;   (function_declaration)
+;   (function_definition)
+;   (function_call)
+;   (table_constructor)
+;   (assignment_statement)
+;   (dot_index_expression)
+;   (string)
+;   (string_content)
+; ] @parent
 ; (expression_list) @list
 (arguments) @list
