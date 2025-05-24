98e015e432c5fc521e40c03e29174c4b47a78070
Matthew George <mattmg412@gmail.com>
2025-05-23 18:06:48 -0700

    feat: enhance breadcrumbs and utils functionality
    Added enable function to breadcrumbs for better control over winbar behavior.
    Updated utils to handle expression_statement in expr_node.

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -373,6 +373,7 @@ M.expr_node = function()
     local ty = node:type()
     if
         ty == "identifer" or
+        ty == "expression_statement" or
         ty == "field_expression" or
         ty == "tuple_expression" or
         ty == "call_expression" or
