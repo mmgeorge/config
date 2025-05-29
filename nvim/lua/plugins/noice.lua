7ac05b210e4d4101b291ec24328a4249a46b105b
Matthew George <mgeorge@esri.com>
2025-05-28 18:22:53 -0700

    fix(noice): update notification settings
    Adjusted icon settings, minimum width, and timeout for better usability.

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,21 +5,21 @@ return {
       background_colour = "NotifyBackground",
       fps = 30,
       icons = {
-        DEBUG = "",
-        ERROR = "",
-        INFO = "",
-        TRACE = "✎",
-        WARN = ""
+        DEBUG = "",
+        ERROR = "",
+        INFO = "",
+        TRACE = "",
+        WARN = ""
       },
       level = 2,
-      minimum_width = 30,
+      minimum_width = 20,
       render = "compact",
       stages = "static",
       time_formats = {
         notification = "%T",
         notification_history = "%FT%T"
       },
-      timeout = 2000,
+      timeout = 6000,
       top_down = false
     },
   },
