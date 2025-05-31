02e997b22143d1ffd3f309fc81a77bbf83592caa
Matthew George <mattmg412@gmail.com>
2025-05-30 18:35:51 -0700

    feat(lualine): add new status components for noice integration

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -208,8 +208,25 @@ return {
                 -- return message
               end,
               cond = require("noice").api.status.message.has,
-            }
+            },
+            {
+              require("noice").api.status.command.get,
+              cond = require("noice").api.status.command.has,
+              color = { fg = "#ff9e64" },
+            },
+            {
+              require("noice").api.status.mode.get,
+              cond = require("noice").api.status.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+            {
+              require("noice").api.status.search.get,
+              cond = require("noice").api.status.search.has,
+              color = { fg = "#ff9e64" },
+            },
+
           },
+
           lualine_c = {
             -- 'branch',
             -- 'diff',
