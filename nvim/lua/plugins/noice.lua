f96dbfbfbe5dc61427c34e25a93e780614fb89d6
Matt George <mattmg412@gmail.com>
2025-05-31 12:17:52 -0700

    feat: Configure LSP, Noice, and Obsidian plugins
    - Updates LSP to use a single border for floats.
    - Adds border to hover docs and signature help in Noice.
    - Configures Obsidian to use a function for the path.

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -82,7 +82,7 @@ return {
         -- mand_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = true,             -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,        -- add a border to hover docs and signature help
+        lsp_doc_border = true,         -- add a border to hover docs and signature help
         view_history = "messages",     -- view for :messages
       },
       popupmenu = {
@@ -103,10 +103,13 @@ return {
           enabled = false
         },
         signature = {
-          enabled = false
+          enabled = false,
+          view = "notify",
+          auto_open = {
+            enabled = true,
+          }
         },
       }
-
     },
   }
 }
