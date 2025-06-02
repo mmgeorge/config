5fd6fc61e25a5c0ebdc922b35ff7dc26c7be978b
Matt George <mattmg412@gmail.com>
2025-06-01 19:37:08 -0700

    feat: Add "Extract Function" prompt and improve AI functionality
    This commit introduces a new "Extract Function" prompt in the
    AI plugin and enhances the existing AI functionality.

--- nvim/lua/plugins/autopair.lua
+++ nvim/lua/plugins/autopair.lua
@@ -2,20 +2,26 @@ return {
   {
     'altermo/ultimate-autopair.nvim',
     event = { 'InsertEnter', 'CmdlineEnter' },
-    branch = 'v0.6',   --recommended as each new version will have breaking changes
+    branch = 'v0.6',     --recommended as each new version will have breaking changes
     opts = {
       internal_pairs = { -- *ultimate-autopair-pairs-default-pairs*
-        { '[',    ']',   fly = true,                                                      dosuround = true, newline = true, space = true },
-        { '(',    ')',   fly = true,                                                      dosuround = true, newline = true, space = true },
-        { '{',    '}',   fly = true,                                                      dosuround = true, newline = true, space = true },
-        { '"',    '"',   suround = true,                                                  multiline = false },
+        { '[', ']', fly = true,     dosuround = true, newline = true, space = true },
+        { '(', ')', fly = true,     dosuround = true, newline = true, space = true },
+        { '{', '}', fly = true,     dosuround = true, newline = true, space = true },
+        { '"', '"', suround = true, multiline = false },
         -- {"'","'",suround=true,cond=function(fn) return not fn.in_lisp() or fn.in_string() end,alpha=true,nft={'tex'},multiline=false},
-        { '`',    '`',   cond = function(fn) return not fn.in_lisp() or fn.in_string() end, nft = { 'tex' }, multiline = false },
+        {
+          '`',
+          '`',
+          -- cond = function(fn) return not fn.in_lisp() or fn.in_string() end,
+          nft = { 'tex' },
+          multiline = false
+        },
         { '``',   "''",  ft = { 'tex' } },
-        { '```',  '```', newline = true,                                                  ft = { 'markdown' } },
-        { '<!--', '-->', ft = { 'markdown', 'html' },                                     space = true },
-        { '"""',  '"""', newline = true,                                                  ft = { 'python' } },
-        { "'''",  "'''", newline = true,                                                  ft = { 'python' } },
+        { '```',  '```', newline = true,              ft = { 'markdown' } },
+        { '<!--', '-->', ft = { 'markdown', 'html' }, space = true },
+        { '"""',  '"""', newline = true,              ft = { 'python' } },
+        { "'''",  "'''", newline = true,              ft = { 'python' } },
       },
       tabout = {
         enable = false,
