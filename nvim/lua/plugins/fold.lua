a9d0f72496eb4e9e7f4b36c2197a7c6f60d1ba99
Matthew George <mattmg412@gmail.com>
2025-07-18 09:53:22 -0700

    feat(fold): add key mapping structure for fold toggle
    Introduced a commented-out key mapping structure for fold toggle functionality.

--- nvim/lua/plugins/fold.lua
+++ nvim/lua/plugins/fold.lua
@@ -1,11 +1,14 @@
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
-  config = function ()
+  keys = {
+    -- { "<leader>k", mode = { "n", "x" }, function() vim.cmd("normal! za") end, desc = "Fold Toggle" },
+  },
+  config = function()
     require('ufo').setup({
       provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
+        return { 'treesitter', 'indent' }
       end
-    }) 
+    })
   end
 }
