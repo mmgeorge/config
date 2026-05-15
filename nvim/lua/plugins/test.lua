ef302c74ad009ad6c09d4e335808003c7d723ff0
Matt George <mattmg412@gmail.com>
2026-05-15 13:26:47 -0700

    feat: add vitest_adapter and keymaps to test.lua

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,3 +1,124 @@
+local function result_suffix(id)
+  return id:match("^[^:]+::(.+)$") or id
+end
+
+local function ends_with_test_suffix(value, suffix)
+  return value == suffix or value:sub(-#suffix - 2) == "::" .. suffix
+end
+
+local function remap_results_to_positions(results, tree)
+  local test_ids = {}
+  local suffix_to_ids = {}
+
+  for _, node in tree:iter_nodes() do
+    local pos = node:data()
+    if pos.type == "test" then
+      local suffix = result_suffix(pos.id)
+      test_ids[#test_ids + 1] = { id = pos.id, suffix = suffix }
+      suffix_to_ids[suffix] = suffix_to_ids[suffix] or {}
+      table.insert(suffix_to_ids[suffix], pos.id)
+    end
+  end
+
+  local function unique_match(matches)
+    if #matches == 1 then
+      return matches[1]
+    end
+  end
+
+  local function find_position_id(result_id)
+    if tree:get_key(result_id) then
+      return result_id
+    end
+
+    local suffix = result_suffix(result_id)
+    local direct = unique_match(suffix_to_ids[suffix] or {})
+    if direct then
+      return direct
+    end
+
+    local matches = {}
+    for _, test_id in ipairs(test_ids) do
+      if ends_with_test_suffix(suffix, test_id.suffix) or ends_with_test_suffix(test_id.suffix, suffix) then
+        table.insert(matches, test_id.id)
+      end
+    end
+
+    return unique_match(matches)
+  end
+
+  local mapped = {}
+  for id, result in pairs(results) do
+    mapped[find_position_id(id) or id] = result
+  end
+
+  return mapped
+end
+
+local function vitest_adapter()
+  local adapter = require("neotest-vitest")({
+    vitestCommand = "pnpm vitest run",
+  })
+
+  local base_results = adapter.results
+  adapter.results = function(spec, process_result, tree)
+    return remap_results_to_positions(base_results(spec, process_result, tree), tree)
+  end
+
+  local query = [[
+    ((call_expression
+      function: (identifier) @func_name (#any-of? @func_name "describe" "suite")
+      arguments: (arguments . (string (string_fragment) @namespace.name))
+    )) @namespace.definition
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "describe" "suite")
+      )
+      arguments: (arguments . (string (string_fragment) @namespace.name))
+    )) @namespace.definition
+    ((call_expression
+      function: (identifier) @func_name (#any-of? @func_name "it" "test")
+      arguments: (arguments . (string (string_fragment) @test.name))
+    )) @test.definition
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "test" "it")
+      )
+      arguments: (arguments . (string (string_fragment) @test.name))
+    )) @test.definition
+  ]]
+
+  adapter.discover_positions = function(path)
+    return require("neotest.lib").treesitter.parse_positions(path, query, { nested_tests = true })
+  end
+
+  local base_build_spec = adapter.build_spec
+  adapter.build_spec = function(args)
+    local spec = base_build_spec(args)
+    if spec then
+      spec.context.tree = args.tree
+
+      if spec.stream then
+        local base_stream = spec.stream
+        spec.stream = function(...)
+          local next_results = base_stream(...)
+          return function()
+            local results = next_results()
+            if not results then
+              return nil
+            end
+
+            return remap_results_to_positions(results, args.tree)
+          end
+        end
+      end
+    end
+    return spec
+  end
+
+  return adapter
+end
+
 return {
   {
     "nvim-neotest/neotest",
@@ -6,14 +127,46 @@ return {
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "marilari88/neotest-vitest",
       -- "mrcjkb/rustaceanvim",
-    }, 
-    config = function ()
-      -- require("neotest").setup({
-      --   adapters = {
-      --     require("rustaceanvim.neotest"),
-      --   },
-      -- })  
+    },
+    keys = {
+      {
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run nearest test",
+      },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%:p"))
+        end,
+        desc = "Run test file",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle test summary",
+      },
+      {
+        "<leader>tu",
+        function()
+          require("neotest").output.open({ enter = true, auto_close = true })
+        end,
+        desc = "Open test output",
+      },
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          vitest_adapter(),
+          -- require("rustaceanvim.neotest"),
+        },
+      })
     end
   }
 }
